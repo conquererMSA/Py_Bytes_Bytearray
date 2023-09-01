a73f26a0a7364f4254daeded3b476cd087575c21
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-09-01 23:06:30 +0600

    Bytes explaination

--- bytesAndByteArray.py
+++ bytesAndByteArray.py
@@ -1,4 +1,14 @@
 '''
 bytes() and bytearray() function they are return byte value of a integer or array of
 integer.
+bytes: bytes is a represents of group of byte values.
+
 '''
+data=[1000]
+# bytesData=bytes(data) #through error
+# print(bytesData) # ValueError: bytes must be in range(0, 256)
+
+dataInt=1000
+bytesDataInt=bytes(dataInt)
+# print(bytesDataInt) # b'\x00\x00\x00\more.....
+
